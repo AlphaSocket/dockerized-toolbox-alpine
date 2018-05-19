183a437a2f7d7fb09d7ef3b0a422dbc6fc50683d
alphasocket <22446807+AlphaSocket@users.noreply.github.com>
2018-05-19 22:46:23 +0100

    Adapted ENV vars

--- imports/nfsd.sh
+++ imports/nfsd.sh
@@ -17,40 +17,42 @@ stop()
   exit
 }
 
-if [ -z "${SHARED_DIRECTORY}" ]; then
-  echo "The SHARED_DIRECTORY environment variable is missing or null, defaulting to /nfsshare "
-  mkdir -p /nfsshare 
+if [ -z "${CONFIG_NFS_SHARED_DIRECTORY}" ]; then
+  echo "The CONFIG_NFS_SHARED_DIRECTORY environment variable is missing or null, defaulting to /var/nfsshare "
+  mkdir -p /var/nfsshare
+  CONFIG_NFS_SHARED_DIRECTORY="/var/nfsshare"
 fi
-if [ -z "${PERMITTED}" ]; then
-  PRIVATE_NETWORKS="10.*.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.16.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.17.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.18.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.19.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.20.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.21.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.22.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.23.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.24.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.25.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.26.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.27.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.28.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.29.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.30.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},172.31.*.*"
-  PRIVATE_NETWORKS="${PRIVATE_NETWORKS},192.168.*.*"
+if [ -z "${CONFIG_NFS_IP_PERMITTED}" ]; then
+  PRIVATE_NETWORKS="127.0.0.1"
+  PRIVATE_NETWORKS+=",10.*.*.*"
+  PRIVATE_NETWORKS+=",172.16.*.*"
+  PRIVATE_NETWORKS+=",172.17.*.*"
+  PRIVATE_NETWORKS+=",172.18.*.*"
+  PRIVATE_NETWORKS+=",172.19.*.*"
+  PRIVATE_NETWORKS+=",172.20.*.*"
+  PRIVATE_NETWORKS+=",172.21.*.*"
+  PRIVATE_NETWORKS+=",172.22.*.*"
+  PRIVATE_NETWORKS+=",172.23.*.*"
+  PRIVATE_NETWORKS+=",172.24.*.*"
+  PRIVATE_NETWORKS+=",172.25.*.*"
+  PRIVATE_NETWORKS+=",172.26.*.*"
+  PRIVATE_NETWORKS+=",172.27.*.*"
+  PRIVATE_NETWORKS+=",172.28.*.*"
+  PRIVATE_NETWORKS+=",172.29.*.*"
+  PRIVATE_NETWORKS+=",172.30.*.*"
+  PRIVATE_NETWORKS+=",172.31.*.*"
+  PRIVATE_NETWORKS+=",192.168.*.*"
     
-  echo "The PERMITTED environment variable is missing or null, defaulting to privat networks."
+  echo "The PERMITTED environment variable is missing or null, defaulting to private networks."
   echo "Only hosts in private networks can mount"
-  PERMITTED=$PRIVATE_NETWORKS
+  CONFIG_NFS_IP_PERMITTED=$PRIVATE_NETWORKS
 fi
-if [ -z "${READ_ONLY}" ]; then
-  echo "The READ_ONLY environment variable is missing or null, defaulting to 'rw'"
+if [ -z "${CONFIG_NFS_READ_ONLY}" ]; then
+  echo "The CONFIG_NFS_READ_ONLY environment variable is missing or null, defaulting to 'rw'"
   echo "Clients have read/write access."
 fi
-if [ -z "${SYNC}" ]; then
-  echo "The SYNC environment variable is missing or null, defaulting to 'async'".
+if [ -z "${CONFIG_NFS_SYNC}" ]; then
+  echo "The CONFIG_NFS_SYNC environment variable is missing or null, defaulting to 'async'".
   echo "Writes will not be immediately written to disk."
 fi
 
